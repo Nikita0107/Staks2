882260402f40b7f2f1a603555631d2b0c9f778c8
niksi <nik.sidoryak@mail.ru>
2024-04-27 15:23:20 +0300

    !!!!

--- кккк.py
+++ кккк.py
@@ -3,23 +3,25 @@ import logging
 import random
 
 # Настройка логирования
-logging.basicConfig(level=logging.DEBUG, filename='py_log.log', filemode='w', format='%(asctime)s %(levelname)s %(message)s')
+logging.basicConfig(level=logging.DEBUG, filename='py_log.log', filemode='w',
+                    format='%(asctime)s %(levelname)s %(message)s')
 
 def random_numbers(min_number, max_number):
+    """Генерация случайного числа, если введенные границы диапазона корректны."""
     if min_number < 0 or max_number < 0:
         logging.warning('Ошибка: границы диапазона не должны быть меньше нуля.')
         print('Недопустимые границы диапазона: числа должны быть ноль или больше!')
-        return
+        return None
     elif min_number >= max_number:
         logging.warning('Ошибка: минимальное значение должно быть меньше максимального.')
         print('Недопустимые границы диапазона: минимальное число должно быть меньше максимального!')
-        return
+        return None
     else:
         return random.randint(min_number, max_number)
 
 while True:
     try:
-        input_values = input('Введите минимальное и максимальное число диапазона через пробел: ')
+        input_values = input('Введите минимальное и максимальное число диапазона через пробел (оба числа должны быть больше нуля): ')
         if not input_values.strip():
             # Пользователь ввел пустую строку
             logging.warning('Ошибка: пустой ввод. Требуется ввести два числа.')
